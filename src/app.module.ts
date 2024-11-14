fbb39a0fac42111182dba71555cfa4471aa8be23
Shevait <shevait.verma@metastart.in>
2024-11-14 17:10:57 +0530

    integrating database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, DatabaseModule, EmployeesModule],
 })
 export class AppModule {}
